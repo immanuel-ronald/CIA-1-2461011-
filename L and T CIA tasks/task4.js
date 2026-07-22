c99f345dc5e68186d0ceaa28b512c28790e2623b
Immanuel Ronald <immanuel.ronald@btech.christuniversity.in>
2026-07-22 12:08:27 +0530

    Update task4.js

--- L and T CIA tasks/task4.js
+++ L and T CIA tasks/task4.js
@@ -8,3 +8,9 @@ let ageDifference = patient1Age - patient2Age;
 console.log("Patient 1 Age:", patient1Age);
 console.log("Patient 2 Age:", patient2Age);
 console.log("Age Difference:", Math.abs(ageDifference), "years");
+
+/* OUTPUT:
+Patient 1 Age: 45
+Patient 2 Age: 29
+Age Difference: 16 years
+*/
